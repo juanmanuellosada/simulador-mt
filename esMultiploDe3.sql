935af36e1e4565f95f74a8af7d17eaf5ae9a389d
Juan Manuel Losada <juanmalosada01@gmail.com>
2024-06-20 22:29:46 -0300

    Arreglo transacciones

--- esMultiploDe3.sql
+++ esMultiploDe3.sql
@@ -1,28 +1,21 @@
--- Inserta el alfabeto en la tabla alfabeto
-TRUNCATE alfabeto;
-INSERT INTO alfabeto (caracter) VALUES ('a'), ('e'), ('i'), ('o'), ('u'), ('('), (')'), ('B');
-
 -- Reinicia la tabla de transiciones para la Máquina de Turing
 TRUNCATE programa CASCADE;
 
 -- Inserta las transiciones en la tabla
-TRUNCATE programa CASCADE;
 INSERT INTO programa (estado_origen, caracter_origen, estado_nuevo, caracter_nuevo, desplazamiento) VALUES
-('q0', '0', 'qf', '0', 'R'),
+('q0', '0', 'q0', '0', 'R'),
 ('q0', '1', 'q1', '1', 'R'),
 
-
 ('q1', '0', 'q2', '0', 'R'),
-('q1', '1', 'qf', '1', 'R'),
-
+('q1', '1', 'q0', '1', 'R'),
 
 ('q2', '0', 'q1', '0', 'R'),
 ('q2', '1', 'q2', '1', 'R'),
 
-('qf', '1', 'q1', '1', 'R');
-
-
-
+-- Transiciones para manejar el carácter blanco al final de la cinta
+('q0', 'B', 'qf', 'B', 'R'), -- Múltiplo de 3
+('q1', 'B', 'q_reject', 'B', 'R'), -- No es múltiplo de 3
+('q2', 'B', 'q_reject', 'B', 'R'); -- No es múltiplo de 3
 
 -- Función para ejecutar la simulación de la Máquina de Turing
 CREATE OR REPLACE FUNCTION ejecutar_esMultiplode3(cinta TEXT) RETURNS SETOF traza_ejecucion AS $$
@@ -35,6 +28,5 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-
 -- Ejemplo de uso:
-SELECT ejecutar_esMultiplode3('1001B');
+SELECT ejecutar_esMultiplode3('1011011101101110B');
